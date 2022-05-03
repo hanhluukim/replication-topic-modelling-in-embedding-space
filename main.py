114938b79ce6f7b238ccc4b2844782e605bb4b60
hanhluukim <luukimbi@gmail.com>
2022-05-04 01:22:27 +0200

    update main.py

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from collections import Counter
 import pandas as pd
 from src.embedding import WordEmbeddingCreator
 from pathlib import Path
-
+from src.train_etm import DocSet, Train
 
 parser = argparse.ArgumentParser(description='main.py')
 parser.add_argument('--model', type=str, default="LDA", help='which topic model should be used')
 #parser.add_argument('--n-sub-test', type=int, default=50, help='n-samples of test-set')
 args = parser.parse_args()
 model_name = args.model
-print(model_name)
 
 # loading
 textsloader = TextDataLoader(source="20newsgroups", train_size=None, test_size=None)
@@ -29,7 +28,7 @@ textsloader.split_and_create_voca_from_trainset(max_df=0.7, min_df=10, stopwords
 print("\n")
 
 for_lda_model = True
-word2id, id2word, train, test, val = textsloader.create_bow_and_savebow_for_each_set(for_lda_model=for_lda_model)
+word2id, id2word, train_set, test_set, val_set = textsloader.create_bow_and_savebow_for_each_set(for_lda_model=for_lda_model)
 #print(train)
 print("\n")
 # BOW-Representation: train['tokens] is the set of unique-word-ids, train['count'] is the token-frequency of each unique-word-id in the document
@@ -39,7 +38,7 @@ if for_lda_model == True:
   print("train representation for LDA")
   print(f'id2word for LDA: {id2word}')
   print(f'example bow-representation for lda: ')
-  for e in train[0]:
+  for e in train_set[0]:
     if e[1]!=0:
       lda_token_ids.append(e[0])
       lda_counts.append(e[1])
@@ -47,24 +46,24 @@ if for_lda_model == True:
   print(lda_counts)
 
 for_lda_model = False
-word2id, id2word, train, test, val = textsloader.create_bow_and_savebow_for_each_set(for_lda_model=for_lda_model)
+word2id, id2word, train_set, test_set, val_set = textsloader.create_bow_and_savebow_for_each_set(for_lda_model=for_lda_model)
 print("train-bow-representation for ETM: \n")
 print(f'id2word for ETM: {id2word}')
 
 print("compare lda and etm representation: \n")
 print(lda_token_ids)
 print(lda_counts)
-print(train['tokens'][0])
-print(train['counts'][0])
+print(train_set['tokens'][0])
+print(train_set['counts'][0])
 
 print(len(lda_token_ids))
-print(len(train['tokens'][0]))
+print(len(train_set['tokens'][0]))
 
 print(100*"=")
 print(f'Size of the vocabulary after prprocessing ist: {len(textsloader.vocabulary)}')
-print(f'Size of train set: {len(train["tokens"])}')
-print(f'Size of val set: {len(val["tokens"])}')
-print(f'Size of test set: {len(test["test"]["tokens"])}')
+print(f'Size of train set: {len(train_set["tokens"])}')
+print(f'Size of val set: {len(val_set["tokens"])}')
+print(f'Size of test set: {len(test_set["test"]["tokens"])}')
 
 
 # example word2id
@@ -84,20 +83,36 @@ train_docs_df['text-after-preprocessing'] = [' '.join(doc) for doc in docs_tr[:1
 print(train_docs_df)
 
 # embedding training
-
 save_path = Path.joinpath(Path.cwd(), "vocab_embedding.txt")
 wb_creator = WordEmbeddingCreator(model_name="cbow", documents = docs_tr, save_path= save_path)
 wb_creator.train(min_count=2, embedding_size= 10)
 vocab = list(word2id.keys())
 wb_creator.create_and_save_vocab_embedding(vocab, save_path)
+
+# embedding word-vectors visualize
 embedding_path = save_path
 fig_path = Path.joinpath(Path.cwd(), "figures")
 wb_creator.cluster_words(embedding_path, fig_path)
 
+# setting parameters for training ETM
+class TrainArguments:
+      def __init__(self, epochs, batch_size, log_interval):
+          self.epochs = epochs
+          self.batch_size = batch_size
+          self.log_interval = log_interval
+class OptimizerArguments:
+      def __init__(self, optimizer_name, lr, wdecay):
+            self.optimizer = optimizer_name
+            self.lr = lr
+            self.wdecay = wdecay
+            
+train_args = TrainArguments(epochs=100, batch_size=100, log_interval=None)
+optimizer_args = OptimizerArguments(optimizer_name="Adam", lr=0.005, wdecay=0.1)
+
+# DocSet test
+vocab_size = len(list(word2id.keys()))
+tr_set = DocSet("train", vocab_size, train_set)
+print(len(tr_set))
+print(tr_set.__getitem__(0))
 
 
-# LDA model
-
-
-
-# ETM model
