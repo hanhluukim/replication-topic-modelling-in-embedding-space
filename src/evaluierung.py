a62f3fc5c6585422cfa3a99f2c2280b5c5288475
EliFritsch <spanien310@yahoo.de>
2022-05-04 13:57:22 +0200

    topic Cohorence

--- src/evaluierung.py
+++ src/evaluierung.py
@@ -16,8 +16,18 @@ for document in documents:
 return p
 
 def marg(w,documents):
+##todo
 return 1
 
 def pointwiseInf(documents,w1,w2,anzDoc):
 	f= math.log(coocurenceP(documents,w1,w2,anzDoc)/(marg(w1,documents)*marg(w2,documents)))/(-math.log(coocurenceP(documents,w1,w2,anzDoc)))
 return f
+
+def topicCoherence(topicsWords,anzahlTopics,documents,anzDoc):
+c=0
+	for topicWords in topicsWords:
+		for i in range(10):
+			for j in range(i+1,10):
+				c=c+pointwiseInf(documents,topicWords[i],topicWords[j],anzDoc)
+c=c/(45*anzahlTopics)
+return c
