00d5bf442b9dcbe39fd7e5f00009b3f03c7ce09a
EliFritsch <spanien310@yahoo.de>
2022-05-04 15:35:51 +0200

    fixing tab misposition in evaluierung.py

--- src/evaluierung.py
+++ src/evaluierung.py
@@ -1,40 +1,40 @@
 import math
 
 def sameDoc(document,w1,w2):
-elif w1 in document:
-	elif w2 in document:
-		return 1
+	elif w1 in document:
+		elif w2 in document:
+			return 1
+		else:
+			return 0
 	else:
 		return 0
-else:
-	return 0
 
 def coocurenceP(documents,w1,w2,anzDoc):
-p=0
-for document in documents:
-	p=p+(sameDoc(document,w1,w2)/anzDoc)
-return p
+	p=0
+	for document in documents:
+		p=p+(sameDoc(document,w1,w2)/anzDoc)
+	return p
 
 def marg(w,documents):
-anzwAll=0
-anzw=0
-for document in documents:
-	anzwAll=anzwAll+len(document)
-	for word in document:
-		if w==word:
-			anzw=anzw+1
+	anzwAll=0
+	anzw=0
+	for document in documents:
+		anzwAll=anzwAll+len(document)
+		for word in document:
+			if w==word:
+				anzw=anzw+1
 
-return anzw/anzwAll
+	return anzw/anzwAll
 
 def pointwiseInf(documents,w1,w2,anzDoc):
 	f= math.log(coocurenceP(documents,w1,w2,anzDoc)/(marg(w1,documents)*marg(w2,documents)))/(-math.log(coocurenceP(documents,w1,w2,anzDoc)))
-return f
+	return f
 
 def topicCoherence(topicsWords,anzahlTopics,documents,anzDoc):
-c=0
-	for topicWords in topicsWords:
-		for i in range(10):
-			for j in range(i+1,10):
-				c=c+pointwiseInf(documents,topicWords[i],topicWords[j],anzDoc)
-c=c/(45*anzahlTopics)
-return c
+	c=0
+		for topicWords in topicsWords:
+			for i in range(10):
+				for j in range(i+1,10):
+					c=c+pointwiseInf(documents,topicWords[i],topicWords[j],anzDoc)
+	c=c/(45*anzahlTopics)
+	return c
