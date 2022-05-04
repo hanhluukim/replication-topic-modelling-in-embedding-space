4a9947fcebffb3f37eb2fec768495a0ceabd9b6b
EliFritsch <spanien310@yahoo.de>
2022-05-04 13:10:51 +0200

    normalized pointwise Information

--- src/evaluierung.py
+++ src/evaluierung.py
@@ -1,3 +1,5 @@
+import math
+
 def sameDoc(document,w1,w2):
 elif w1 in document:
 	elif w2 in document:
@@ -6,8 +8,16 @@ elif w1 in document:
 		return 0
 else:
 	return 0
-def coocrenceP(documents,w1,w2,anzDoc):
+
+def coocurenceP(documents,w1,w2,anzDoc):
 p=0
 for document in documents:
 	p=p+(sameDoc(document,w1,w2)/anzDoc)
 return p
+
+def marg(w,documents):
+return 1
+
+def pointwiseInf(documents,w1,w2,anzDoc):
+	f= math.log(coocurenceP(documents,w1,w2,anzDoc)/(marg(w1,documents)*marg(w2,documents)))/(-math.log(coocurenceP(documents,w1,w2,anzDoc)))
+return f
