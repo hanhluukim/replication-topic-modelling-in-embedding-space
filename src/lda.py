2a73b91b02408f062a7bad17fb7f404453883de8
EliFritsch <spanien310@yahoo.de>
2022-05-04 15:34:09 +0200

    fixing lda tab and word mispositions

--- src/lda.py
+++ src/lda.py
@@ -1,5 +1,5 @@
 from gensim.models import LdaModel
 
 def lda(corpus,anzTopics,id2word):
-lda=LdaModel(corpus,id2word=id2word,numTopics=anzTopics)
-return lda
+	lda=LdaModel(corpus,id2word=id2word,num_topics=anzTopics)
+	return lda
