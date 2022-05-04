917ce4fad4637d1caf862b3bd70c99b498dbce18
EliFritsch <spanien310@yahoo.de>
2022-05-04 14:29:51 +0200

    marginal word

--- src/evaluierung.py
+++ src/evaluierung.py
@@ -16,8 +16,15 @@ for document in documents:
 return p
 
 def marg(w,documents):
-##todo
-return 1
+anzwAll=0
+anzw=0
+for document in documents:
+	anzwAll=anzwAll+len(document)
+	for word in document:
+		if w==word:
+			anzw=anzw+1
+
+return anzw/anzwAll
 
 def pointwiseInf(documents,w1,w2,anzDoc):
 	f= math.log(coocurenceP(documents,w1,w2,anzDoc)/(marg(w1,documents)*marg(w2,documents)))/(-math.log(coocurenceP(documents,w1,w2,anzDoc)))
